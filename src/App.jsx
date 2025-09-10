fd4ebb68481159ea5bda8b5229b6f74facf1a0a1
suhrobquldoshev58 <suhrobquldoshev58@gmail.com>
2025-09-10 12:33:50 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <body>
+<header>
+  <img src="" alt="" />
+  <ul>
+   <li><a href="">home</a></li>
+   <li><a href="">menu</a></li>
+   <li><a href="">Services</a></li>
+   <li><a href="">Offers</a></li>
+  </ul>
+
+  <div>
+    <i class="fa-brands fa-searchengin"></i>
+    <button> <i class="fa-solid fa-phone-volume"></i>  Contact</button>
+  </div>
+</header>
+
+
+
+
+
+    </body>
+  )
+}
+
+export default App
